09bcda8a0ea23dd57e55375441188cd945053ec6
Evgeniy Petrovich <evgeniy.petrovich@skillbox.ru>
2021-11-06 21:08:10 +0300

    no migration directory

--- db/migrations/000004_create_base_table.up.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS "bases" (
-  "id" bigserial,
-  "uuid" char(36) UNIQUE,
-  "created_at" timestamptz DEFAULT CURRENT_TIMESTAMP,
-  "updated_at" timestamptz DEFAULT CURRENT_TIMESTAMP,
-  "deleted_at" timestamptz,
-  PRIMARY KEY ("id")
-)
